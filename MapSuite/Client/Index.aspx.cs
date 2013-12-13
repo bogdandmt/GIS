a0624b540b693a7e1b68840ae903404a432ba927
bogdandmt <bogdandmt@gmail.com>
2013-12-13 23:24:12 +0200

    Created new version of the project

---
+++ MapSuite/Client/Index.aspx.cs
@@ -0,0 +1,46 @@
+﻿using Server;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ThinkGeo.MapSuite.Core;
+using ThinkGeo.MapSuite.WebEdition;
+
+namespace Client
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String dataPath = Directory.GetParent(Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).FullName).FullName + @"\Server\Data\";
+            MainMap.MapUnit = GeographyUnit.DecimalDegree;
+
+            ShapeFileFeatureLayer worldLayer = ServerFactory.CreateWorldLayer();
+            worldLayer.ZoomLevelSet.ZoomLevel01.DefaultAreaStyle = AreaStyles.Country1;
+            worldLayer.ZoomLevelSet.ZoomLevel01.ApplyUntilZoomLevel = ApplyUntilZoomLevel.Level20;
+
+            ShapeFileFeatureLayer capitalPointLayer = ServerFactory.CreateCapitalPointLayer();
+            capitalPointLayer.ZoomLevelSet.ZoomLevel01.DefaultPointStyle = PointStyles.Capital3;
+            capitalPointLayer.ZoomLevelSet.ZoomLevel01.ApplyUntilZoomLevel = ApplyUntilZoomLevel.Level20;
+
+            ShapeFileFeatureLayer capitalNameLayer = ServerFactory.CreateCapitalNameLayer();
+            capitalNameLayer.ZoomLevelSet.ZoomLevel01.DefaultTextStyle = TextStyles.Capital3("CITY_NAME");
+            capitalNameLayer.ZoomLevelSet.ZoomLevel01.ApplyUntilZoomLevel = ApplyUntilZoomLevel.Level20;
+            capitalNameLayer.DrawingMarginPercentage = 50;
+
+
+            MainMap.StaticOverlay.Layers.Add(worldLayer);
+            MainMap.StaticOverlay.Layers.Add(capitalPointLayer);
+            MainMap.StaticOverlay.Layers.Add(capitalNameLayer);
+
+            MainMap.CurrentExtent = new RectangleShape(5, 78, 30, 26);
+
+            MainMap.MapBackground.BackgroundBrush = new GeoSolidBrush(GeoColor.GeographicColors.ShallowOcean);
+
+            ServerFactory c = new ServerFactory(); ;
+        }
+    }
+}
---
+++ MapSuite/Server/ServerFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThinkGeo.MapSuite.Core;
+
+namespace Server
+{
+    public class ServerFactory
+    {
+        private static String dataPath = Directory.GetParent(Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).FullName).FullName + @"\Server\Data\";
+        
+        public static ShapeFileFeatureLayer CreateWorldLayer()
+        {
+            ShapeFileFeatureLayer worldLayer = new ShapeFileFeatureLayer(dataPath + @"World\cntry02.shp");
+            return worldLayer;
+        }
+
+        public static ShapeFileFeatureLayer CreateCapitalPointLayer()
+        {
+            ShapeFileFeatureLayer capitalPointLayer = new ShapeFileFeatureLayer(dataPath + @"World\capital.shp");
+            return capitalPointLayer;
+        }
+
+        public static ShapeFileFeatureLayer CreateCapitalNameLayer()
+        {
+            ShapeFileFeatureLayer capitalNameLayer = new ShapeFileFeatureLayer(dataPath + @"World\capital.shp");
+            return capitalNameLayer;
+        }
+    }
+}
