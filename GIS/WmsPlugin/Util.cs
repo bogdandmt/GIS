6ddcdc8eba86b3fcb0a830734381d15d11f785bb
bogdandmt <bogdandmt@gmail.com>
2014-01-12 00:36:31 +0200

    Fixed git issue

---
+++ GIS/WmsPlugin/Util.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WmsPlugin
+{
+    public class Util
+    {
+        public static String dataPath = Directory.GetParent(Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).FullName).FullName + @"\SampleData\";
+        public static String citiesShpFile = dataPath + @"Ukraine\places.shp";
+        public static String roadsShpFile = dataPath + @"Ukraine\roads.shp";
+        public static String poisShpFile = dataPath + @"Ukraine\pois.shp";
+    }
+}
